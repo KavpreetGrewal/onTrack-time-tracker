6ad3608ace6c68634afacaa4ace9f5194c9c3e79
KavpreetGrewal <kavpreetgrewal@gmail.com>
2020-08-05 18:35:31 -0700

    update calender stats

---
+++ lib/calendarPage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class Calendar extends StatefulWidget {
+  @override
+  _CalendarState createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calendar'),
+      ),
+    );
+  }
+}
