3c0fd5d41a57d3da32cc24499134e6af1ecd1e91
KavpreetGrewal <kavpreetgrewal@gmail.com>
2020-08-06 21:21:19 -0700

    add theme colors

---
+++ lib/theme/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ThemeColors  {
+  static const Color LightYellow = Color(0xFFFFF9EC);
+  static const Color LightYellow2 = Color(0xFFFFE4C7);
+  static const Color DarkYellow = Color(0xFFF9BE7C);
+  static const Color PalePink = Color(0xFFFED4D6);
+
+  static const Color Red = Color(0xFFE46472);
+  static const Color Lavender = Color(0xFFD5E4FE);
+  static const Color Blue = Color(0xFF6488E4);
+  static const Color LightGreen = Color(0xFFD9E6DC);
+  static const Color Green = Color(0xFF309397);
+
+  static const Color DarkBlue = Color(0xFF0D253F);
+}
