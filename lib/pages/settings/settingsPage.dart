d28658146dac4b62b2f914d3c0a3fc09cb9cb6bb
KavpreetGrewal <kavpreetgrewal@gmail.com>
2020-08-07 22:50:22 -0700

    update homepage design

--- lib/pages/settings/settingsPage.dart
+++ lib/pages/settings/settingsPage.dart
@@ -1,1 +1,24 @@
 import 'package:flutter/material.dart';
+
+class SettingsVar {
+  static var timeFrame = 'hour';
+  static var timeFrameAdj = 'Hourly';
+  static var period = 'week';
+  static var periodAdj = 'Weekly';
+  static var totalTimePeriod = 70;
+  static var progressOfTimePeriod = 10;
+  static var currentTimePeriod = 0;
+  static var rollingPeriod = true;
+}
+
+class Settings extends StatefulWidget {
+  @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
