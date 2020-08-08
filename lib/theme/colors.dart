7a01d15cd7822f778b2b5ec3d87c1720f460009e
KavpreetGrewal <kavpreetgrewal@gmail.com>
2020-08-07 22:50:28 -0700

    update homepage design

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -6,11 +6,15 @@ class ThemeColors  {
   static const Color DarkYellow = Color(0xFFF9BE7C);
   static const Color PalePink = Color(0xFFFED4D6);
 
-  static const Color Red = Color(0xFFE46472);
   static const Color Lavender = Color(0xFFD5E4FE);
-  static const Color Blue = Color(0xFF6488E4);
   static const Color LightGreen = Color(0xFFD9E6DC);
   static const Color Green = Color(0xFF309397);
 
-  static const Color DarkBlue = Color(0xFF0D253F);
+
+  static const Color LightBlue = Color(0xFFA8D0E6);
+  static const Color Blue = Color(0xFF384785);
+  static const Color DarkBlue = Color(0xFF24305E);
+  static const Color Red = Color(0xFFF76E6C);
+  static const Color White = Color(0xFFF2F2F2);
+
 }
