8e4012202b1f600d722d632af0de0f01ccaf3807
KavpreetGrewal <kavpreetgrewal@gmail.com>
2020-08-09 22:48:07 -0700

    add imports

--- lib/pages/account/accountPage.dart
+++ lib/pages/account/accountPage.dart
@@ -1,1 +1,17 @@
 import 'package:flutter/material.dart';
+
+class Account extends StatefulWidget {
+  @override
+  _AccountState createState() => _AccountState();
+}
+
+class _AccountState extends State<Account> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Account'),
+      ),
+    );
+  }
+}
