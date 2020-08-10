b88f2347c32fb4c8d6bd54619c9b36a8ccbc2bdc
KavpreetGrewal <kavpreetgrewal@gmail.com>
2020-08-09 22:47:24 -0700

    update date variables

--- lib/pages/settings/settingsPage.dart
+++ lib/pages/settings/settingsPage.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import '../settings/variables.dart';
 
+
 class Settings extends StatefulWidget {
   @override
   _SettingsState createState() => _SettingsState();
 }
 
 class _SettingsState extends State<Settings> {
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+
+    );
   }
 }
