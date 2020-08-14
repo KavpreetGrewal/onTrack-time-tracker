2cfa63a31f96f79f915e57c89e101b4362cbe6fb
KavpreetGrewal <kavpreetgrewal@gmail.com>
2020-08-13 23:41:02 -0700

    add firebase auth

---
+++ lib/firebase/firebaseAuth.dart
@@ -0,0 +1,97 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:ontrack_time_tracker/pages/account/loginPage.dart';
+import 'package:ontrack_time_tracker/pages/settings/variables.dart';
+
+class AuthProvider {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  static var error;
+
+  Future<bool> signInWithEmail(String email, String password) async {
+    try {
+      AuthResult result = await _auth.signInWithEmailAndPassword(email: email, password: password);
+      SettingsVar.setUser(result.user);
+      if (SettingsVar.user != null) {
+        return true;
+      } else {
+        error = "Failed sing in, please try again later.";
+        return false;
+      }
+    } catch (e) {
+
+      error = e.toString() == null || e.toString() == '' ?
+      "Failed sign in, please try again later." : e.toString();
+      return false;
+    }
+  }
+
+  Future<void> logOut() async {
+    try {
+      await _auth.signOut();
+    } catch (e) {
+      error = error = e.toString() == null || e.toString() == '' ?
+      "Error logging out, please try again later." : e.toString();
+    }
+  }
+
+  Future<bool> loginWithGoogle() async {
+    try {
+      GoogleSignIn googleSignIn = GoogleSignIn();
+      GoogleSignInAccount account = await googleSignIn.signIn();
+      SettingsVar.setGoogleAccount(account);
+      if (account == null) {
+        error = "Failed sign in with Google, please try again later.";
+        return false;
+      }
+      AuthResult res = await _auth.signInWithCredential(GoogleAuthProvider.getCredential(
+          idToken: (await account.authentication).idToken,
+          accessToken: (await account.authentication).accessToken));
+      if (res.user == null) {
+        error = "Failed Sign In, please try again later.";
+        return false;
+      }
+      return true;
+    } catch (e) {
+      error = error = e.toString() == null || e.toString() == '' ?
+      "Error logging out, please try again later." : e.toString();
+      return false;
+    }
+  }
+
+  Future<bool> signUpWithEmail (String email, String password) async {
+    try {
+      AuthResult result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      SettingsVar.setUser(result.user);
+      if (result.user != null) {
+        return true;
+      } else {
+        error = "Failed sign up, please try again later";
+        return false;
+      }
+    } catch (e) {
+      error = e.toString() == null || e.toString() == '' ?
+      "Error logging out, please try again later." : e.toString();
+      return false;
+    }
+  }
+
+  Future<bool> signInAnon () async {
+    try {
+      AuthResult result = await _auth.signInAnonymously();
+      SettingsVar.setUser(result.user);
+      if (result.user != null) {
+        return true;
+      } else {
+        error = "Failed sign up, please try again later";
+        return false;
+      }
+    } catch (e) {
+      error = e.toString() == null || e.toString() == '' ?
+      "Error logging out, please try again later." : e.toString();
+      return false;
+    }
+  }
+
+}
+
